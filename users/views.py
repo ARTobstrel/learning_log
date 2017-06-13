da964854395780dae6ff5df548e96455006b5ff6
artem <artemfap@yandex.ru>
2017-06-13 23:04:55 +0300

    add logout url

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+
+def logout_view(request):
+    """Завершает сеанс работы с приложением"""
+    logout(request)
+    return HttpResponseRedirect(reverse('learning_logs:index'))
